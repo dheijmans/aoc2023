f7c982977c51894e1bf0df3a781e2a180b99959f
Dennis Heijmans <d.heijmans2019@gmail.com>
2023-12-10 12:29:52 +0100

    Solve day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,3 +1,4 @@
+use num::Integer;
 use std::collections::HashMap;
 
 advent_of_code::solution!(8);
@@ -34,8 +35,44 @@ fn calculate_steps(instructions: Vec<char>, network: HashMap<String, Node>) -> O
     Some(steps)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (chars, nodes) = input.split_once("\n\n")?;
+    let instructions: Vec<char> = chars.chars().collect();
+    let mut network: HashMap<String, Node> = HashMap::new();
+    nodes.lines().for_each(|line| {
+        let id = line[0..3].to_string();
+        let left = line[7..10].to_string();
+        let right = line[12..15].to_string();
+        network.insert(id.clone(), Node::new(id, left, right));
+    });
+    calculate_ghost_steps(instructions, network)
+}
+
+fn calculate_ghost_steps(instructions: Vec<char>, network: HashMap<String, Node>) -> Option<u64> {
+    let start_nodes: Vec<&Node> = network
+        .iter()
+        .map(|entry| entry.1)
+        .filter(|&node| node.id.ends_with('A'))
+        .collect();
+    let total_steps = start_nodes.iter().fold(1, |acc, &x| {
+        let mut ptr = 0;
+        let mut current = x;
+        let mut steps = 0;
+        loop {
+            if current.id.ends_with('Z') {
+                break;
+            }
+            current = match instructions[ptr] {
+                'L' => network.get(&current.left).expect("Left Node"),
+                'R' => network.get(&current.right).expect("Right Node"),
+                _ => panic!(),
+            };
+            steps += 1;
+            ptr = (ptr + 1) % instructions.len();
+        }
+        acc.lcm(&steps)
+    });
+    Some(total_steps)
 }
 
 struct Node {
