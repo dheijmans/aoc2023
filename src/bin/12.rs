9ccf5bc2e2108e95507d25c4284a7ca109d1d7af
Dennis Heijmans <d.heijmans2019@gmail.com>
2023-12-12 21:57:04 +0100

    Format day 12

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -6,12 +6,8 @@ pub fn part_one(input: &str) -> Option<u64> {
         let (mut conditions, sizes) = line.split_once(' ').map(|(a, b)| (String::from(a), b))?;
         conditions.push('.');
         let sizes: Vec<usize> = sizes.split(',').map(|x| x.parse().unwrap()).collect();
-        sum += count_arrangements(
-            conditions.as_str(),
-            &sizes,
-            0,
-            &mut vec![vec![None; sizes.len() + 1]; conditions.len() + 1],
-        )?
+        let mut cache = vec![vec![None; sizes.len() + 1]; conditions.len() + 1];
+        sum += count_arrangements(conditions.as_str(), &sizes, 0, &mut cache)?
     }
     Some(sum)
 }
@@ -20,25 +16,21 @@ pub fn part_two(input: &str) -> Option<u64> {
     let mut sum = 0;
     for line in input.lines() {
         let (conditions, sizes) = unfold(line)?;
-        sum += count_arrangements(
-            conditions.as_str(),
-            &sizes,
-            0,
-            &mut vec![vec![None; sizes.len() + 1]; conditions.len() + 1],
-        )?
+        let mut cache = vec![vec![None; sizes.len() + 1]; conditions.len() + 1];
+        sum += count_arrangements(conditions.as_str(), &sizes, 0, &mut cache)?
     }
     Some(sum)
 }
 
 fn unfold(input: &str) -> Option<(String, Vec<usize>)> {
-    let splits = input.split_once(' ')?;
-    let original_conditions = splits.0;
+    let split = input.split_once(' ')?;
+    let original_conditions = split.0;
     let mut conditions: String = std::iter::repeat(original_conditions)
         .take(5)
         .collect::<Vec<&str>>()
         .join("?");
     conditions.push('.');
-    let original_sizes: Vec<usize> = splits.1.split(',').map(|x| x.parse().unwrap()).collect();
+    let original_sizes: Vec<usize> = split.1.split(',').map(|x| x.parse().unwrap()).collect();
     let mut sizes: Vec<usize> = Vec::new();
     for _ in 0..5 {
         sizes.extend(&original_sizes);
@@ -58,24 +50,21 @@ fn count_arrangements(
     }
 
     if index >= sizes.len() {
-        match conditions.contains('#') {
-            false => {
-                cache[i][j] = Some(1);
-                return Some(1);
-            }
-            true => {
-                cache[i][j] = Some(0);
-                return Some(0);
-            }
-        }
+        let result = match conditions.contains('#') {
+            false => Some(1),
+            true => Some(0),
+        };
+        cache[i][j] = result;
+        return result;
     }
 
     let current = sizes[index];
     let mut arrangements: u64 = 0;
 
     if current > conditions.len() {
-        cache[i][j] = Some(0);
-        return Some(0);
+        let result = Some(0);
+        cache[i][j] = result;
+        return result;
     }
 
     for i in 0..=(conditions.len() - current) {
@@ -85,12 +74,11 @@ fn count_arrangements(
             arrangements +=
                 count_arrangements(&conditions[(i + current + 1)..], sizes, index + 1, cache)?;
         }
-        if conditions.as_bytes()[i].eq(&b'#')
-            || conditions.len() - i - 1 <= sizes[index..].iter().sum()
-        {
+        if conditions.as_bytes()[i].eq(&b'#') {
             break;
         }
     }
-    cache[i][j] = Some(arrangements);
-    Some(arrangements)
+    let result = Some(arrangements);
+    cache[i][j] = result;
+    result
 }
